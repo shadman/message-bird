c7b8a88b72d5e15355ca67a9f0a6fdd4b6f4d9e0
Shadman Jamil <shadman.jamil@tenpearls.com>
2018-08-19 08:20:19 +0500

    initialized queue classes; added config params with hardcoded values

--- classes/messages.php
+++ classes/messages.php
@@ -1,31 +1,38 @@
 <?php
 require_once('config/config.php');
-
-print_r($config);
+require_once('classes/queue.php');
 
 class Message {
 
 	public function __construct(){
-		$this->maxMassageSize = 150;
+
+		$this->maxMassageSize = Config::params('max_size');
+
 	}
 
+	/*
+	* Sending message
+	*/
 	public function send($request){
-		print_r($request);
-
 		$messages = $this->filterMessage($request['message']);
 		$response = $this->sendMessage($messages);
-		if ($response=='sent') {
-			header('Location: index.php?status=sent');
+		if ($response==Config::params('status')->sent) {
+			//header('Location: index.php?status='.Config::params('status')->sent);
 		}
 	}
 
+	/*
+	* Get either message successed of failed per provided status
+	*/
 	public function getMessage($status='') {
 		$message = '';
 
-		if ($status=='sent') $message = "Message has been sent successfully";
+		if ($status==Config::params('status')->sent) $message = "Message has been sent successfully";
 		
 		return $message;
 	}
+
+
 
 
 	private function filterMessage($message=''){
@@ -51,11 +58,15 @@ class Message {
 		return $message_parts;
 	}
 
+
 	private function sendMessage($messages){
-		return 'sent';
+		Queue::message();
+		return Config::params('status')->sent;
 	}
+
 
 	private function removeInvalidCharacters($message=''){
 		return  strip_tags($message);
 	}
+
 }
---
+++ classes/queue.php
@@ -0,0 +1,14 @@
+<?php
+require_once('config/config.php');
+
+class Queue {
+
+	public function __construct(){
+	
+	}
+
+	public function message(){
+		
+	}
+
+}
