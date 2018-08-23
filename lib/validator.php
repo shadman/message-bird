3eaf70780aa52c35f57da7f14a3cbf913d801805
Shadman Jamil <shadman.jamil@tenpearls.com>
2018-08-23 12:26:52 +0500

    added daily limit per IP; configurable from config

--- lib/validator.php
+++ lib/validator.php
@@ -1,7 +1,12 @@
 <?php
+require_once('config/config.php');
 
 class Validator {
 
+
+	/*
+	* Validate client request, before sending any message
+	*/
 	public function validate($request){
 
 		$message = '';
@@ -14,10 +19,44 @@ class Validator {
 			!is_numeric($request['country_code']) ) $message = "Invalid country code found";
 		else if ( strlen($request['cellphone_number']) < Config::params('min_cellphone_number')  || 
 			!is_numeric($request['cellphone_number']) ) $message = "Invalid cellphone number found";
-			
-		if ($message === '') return true;
 		
+
+		if ($message === '') {
+
+			// Check daily limit for specific IP
+			$maxMessageIPDailyLimit = Config::params('ip_daily_message_limit');
+			echo $countOfMessage = self::getCountOfMessages();
+			if ($countOfMessage >= $maxMessageIPDailyLimit ) 
+				return "You have exceeded your daily limit, as $maxMessageIPDailyLimit messages per day are allowed.";
+
+			return true;
+		}
+
 		return $message;
+	}
+
+
+
+	/* 
+	Private 
+	*/
+
+	/*
+	* Get a count of messages for specific IP
+	*/
+	private function getCountOfMessages(){
+		
+		$ipAddress = $_SERVER['REMOTE_ADDR'];
+		$database = new Database;
+		$query = $database->getCount('message_queues', 
+									 " ip_address='$ipAddress' AND DATE(`created_at`) = CURDATE() ");
+		$exec = $database->query($query);
+		if ($exec) {
+			$row = $database->fetchArray($exec);
+			return $row['count'];
+		}
+
+		return 0;
 	}
 
 }
