6aaf79493144bbf69612c652ccd5fdda2f89909a
Shadman Jamil <shadman.jamil@tenpearls.com>
2018-08-26 16:53:14 +0500

    updated condition, to handle get param

--- job.php
+++ job.php
@@ -3,7 +3,8 @@ require_once('config/config.php');
 require_once('jobs/sendMessage.php');
 
 	// Jobs will be executed from here, via crontab
-	switch ($_GET['action']) {
+	$action = ( isset($_GET['action']) ) ? $_GET['action'] : 'default';
+	switch ($action) {
 		case 'send_message':
 			// Job to send messages, pick only 1 message per second
 			$sendMessage = new SendMessages;
